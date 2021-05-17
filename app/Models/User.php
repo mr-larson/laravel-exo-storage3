8f9eb393554dfceed6a70d6ab4974270fd64bc8f
gauthier dewit <gauthierdewit@gmail.com>
2021-05-17 11:49:58 +0200

    tables User ok

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'nom',
+        'prenom',
+        'age',
         'email',
         'password',
+        'photo'
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom',30);
+            $table->string('prenom',30);
+            $table->unsignedInteger('age');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo');
             $table->rememberToken();
             $table->timestamps();
         });
