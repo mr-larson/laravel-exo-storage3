bab040406d2a0afadc5768c9a5ddc6811070ceeb
gauthier dewit <gauthierdewit@gmail.com>
2021-05-17 11:11:57 +0200

    php admin ok

---
+++ app/Http/Controllers/CaracteristiqueController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Caracteristique;
+use Illuminate\Http\Request;
+
+class CaracteristiqueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Caracteristique  $caracteristique
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Caracteristique $caracteristique)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Caracteristique  $caracteristique
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Caracteristique $caracteristique)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Caracteristique  $caracteristique
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Caracteristique $caracteristique)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Caracteristique  $caracteristique
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Caracteristique $caracteristique)
+    {
+        //
+    }
+}
